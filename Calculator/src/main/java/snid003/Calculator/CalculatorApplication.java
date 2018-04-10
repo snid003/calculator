5e757ef17dad28fbf409d6957138ee934fe2bfdf
kimchi <wonyc5030@naver.com>
2018-04-10 15:55:00 +0900

    Add spring boot skeleton

---
+++ Calculator/src/main/java/snid003/Calculator/CalculatorApplication.java
@@ -0,0 +1,12 @@
+package snid003.Calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorApplication.class, args);
+	}
+}

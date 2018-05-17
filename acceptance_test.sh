ed65b0f1245a15eb1b284bdd04fe3a8d536c9aef
snid003 <astana162@gmail.com>
2018-05-17 14:37:41 +0900

    acceptance

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 test $(curl localhost:8765/sum?a=1\&b=2_ -eq 3 || \
-test $(curl $(docker network inspect -f '{{range .IPAM Config}}{{.Gatway}}{{end}}'bridge):8765/sum?a=1\&b=3)
+test $(curl $(docker network inspect -f '{{range .IPAM Config}}{{.Gateway}}{{end}}'bridge):8765/sum?a=1\&b=3)
